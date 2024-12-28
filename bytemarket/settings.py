d4c5ddbd6f6aeb82fdc3e765e4dac3113bfd7fd6
Hossein Nouraie <150235679+Ho3ein02@users.noreply.github.com>
2024-12-28 07:44:19 +0330

    Update settings.py

--- bytemarket/settings.py
+++ bytemarket/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os.path
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,14 +88,18 @@ WSGI_APPLICATION = 'bytemarket.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'bytemarket-database',
+#         'USER': 'root',
+#         'PASSWORD': 'imXJtN8ZMZvo9TAl8poiW22s',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bytemarket-database',
-        'USER': 'root',
-        'PASSWORD': 'imXJtN8ZMZvo9TAl8poiW22s',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 # Password validation
